39c8ef6d84ac1c908e540988c632e06d9ce1745f
Oscar Lindberg <lios22hc@student.ju.se>
2023-10-26 13:37:04 +0200

    Added more buttons

--- app/src/main/java/com/example/notesapp/MainActivity.kt
+++ app/src/main/java/com/example/notesapp/MainActivity.kt
@@ -3,6 +3,10 @@ package com.example.notesapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -13,6 +17,14 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.ui.unit.dp
 
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
@@ -23,34 +35,77 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            AddNoteButton()
+            MyScreen()
         }
     }
 }
 
-//When I made the following function i got help from https://developer.android.com/jetpack/compose/tutorial
-//and ChatGPT: https://chat.openai.com/c/9ab7f4c0-8f93-479d-9220-b90e2e744f15
-@Composable
-fun AddNoteButton() {
-    Row(modifier = Modifier.padding(all = 8.dp)) {
+data class Note(val name: String)
 
-            Button(
-                onClick = { /*TODO*/ },
-                modifier = Modifier
-                    .size(120.dp)
-            ) {
-                Text(
-                    text = "Add note",
-                    style = TextStyle(
-                        color = Color.White,
-                        fontSize = 16.sp,
-                        fontWeight = FontWeight.Bold
-                    ),
-                    textAlign = TextAlign.Center
-                )
-            }
+//When I made the following function i got help from https://developer.android.com/jetpack/compose/tutorial
+//and ChatGPT: https://chat.openai.com/c/5b4da3b5-0596-4c01-a965-9f1ec9eb1842
+@Composable
+fun MyScreen() {
+    val notes = remember { mutableListOf(Note("Note 1"), Note("Note 2"), Note("Note 3")) } // Create a list of notes
+
+    Column {
+        AddNoteButton()
+        NoteList(notes = notes)
     }
 }
+
+@Composable
+fun AddNoteButton() {
+    Button(
+        onClick = {  },
+        modifier = Modifier
+            .height(100.dp)
+            .fillMaxWidth()
+            .padding(8.dp)
+    ) {
+        Text(
+            text = "Add Note",
+            style = TextStyle(
+                color = Color.White,
+                fontSize = 16.sp,
+                fontWeight = FontWeight.Bold
+            ),
+            textAlign = TextAlign.Center
+        )}
+}
+
+@Composable
+fun AddNote(text: String){
+    Button(
+        onClick = {  },
+        modifier = Modifier
+            .height(100.dp)
+            .fillMaxWidth()
+            .padding(8.dp)
+    ) {
+        Text(
+            text = text,
+            style = TextStyle(
+                color = Color.White,
+                fontSize = 16.sp,
+                fontWeight = FontWeight.Bold
+            ),
+            textAlign = TextAlign.Center
+        )}
+}
+
+
+@Composable
+fun NoteList(notes: List<Note>) {
+    Column {
+        notes.forEach{ note: Note ->
+            AddNote(note.name)
+        }
+    }
+
+}
+
+
 
 
 
